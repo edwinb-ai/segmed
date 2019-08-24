43cc5b7e2a0ece479f69a5e51e5d9b1f4143327d
Edwin <developeredwin@gmail.com>
2019-08-23 21:02:52 -0500

    Ajustes adicionales a las métricas y el entrenamiento

--- entrenamiento.py
+++ entrenamiento.py
@@ -24,23 +24,20 @@ x_patches, y_patches = prep.muchas_imagenes_en_partes(
 )
 
 # Hacer aumento de datos simple
-# Hacer aumento de datos simple
 transformaciones = dict(
-    zca_whitening=True,
     # randomly rotate images in the range (deg 0 to 180)
-    rotation_range=50.0,
+    rotation_range=10.0,
     # randomly shift images horizontally
     width_shift_range=0.02,
     # randomly shift images vertically
     height_shift_range=0.02,
-    brightness_range=(0.1, 0.9),
     # set range for random shear
     shear_range=5,
     # set range for random zoom
     zoom_range=0.2,
     fill_mode="nearest",
     horizontal_flip=True,
-    vertical_flip=True,
+    vertical_flip=False,
     validation_split=0.2
 )
 datagen_x = ImageDataGenerator(**transformaciones)
