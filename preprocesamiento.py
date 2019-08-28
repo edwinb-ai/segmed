2079a48d1707aa78913d0a311e30c268bf7eaa90
Edwin <developeredwin@gmail.com>
2019-08-28 15:35:55 -0500

    Se agregó documentación a las utilidades de preprocesamiento.

--- preprocesamiento.py
+++ preprocesamiento.py
@@ -5,13 +5,31 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator
 
 
-def extraer_datos(train_path, label_path, rgb=False, show=False):
+def extraer_datos(train_path, label_path, rgb=False, show=False, tiff=True):
+    """
+    Extrae los datos de train_path y label_path y los normaliza.
+
+    Argumentos:
+        train_path: String que tiene el camino completo del directorio donde están las imágenes.
+        label_path: String que tiene el camino completo del directorio donde están los mapas de segmentación.
+        rgb: Booleano para reestructurar a forma de las imágenes si son de color o grises.
+        show: Booleano, si es verdadero muestra una imagen aleatoria de las que se están importando.
+        tiff: Booleando, si es verdadero es porque las imágenes están en formato .tif.
+
+    Regresa:
+        X_train, y_train: Arreglos de numpy que contienen las imágenes importadas.
+    """
     # Definir el camino de las imágenes
     path_imagenes_entrenamiento = train_path
     path_etiquetas_entrenamiento = label_path
+
     # Importar las imágenes en formato tif
-    X_train = io.imread(path_imagenes_entrenamiento, plugin="tifffile")
-    y_train = io.imread(path_etiquetas_entrenamiento, plugin="tifffile")
+    if tiff:
+        X_train = io.imread(path_imagenes_entrenamiento, plugin="tifffile")
+        y_train = io.imread(path_etiquetas_entrenamiento, plugin="tifffile")
+    else:
+        X_train = io.imread(path_imagenes_entrenamiento)
+        y_train = io.imread(path_etiquetas_entrenamiento)
     # Reacomodar el arreglo para que tenga canales de color
     if rgb:
         # Se agregan los tres canales
@@ -41,11 +59,25 @@ def extraer_datos(train_path, label_path, rgb=False, show=False):
 
     return X_train, y_train
 
-def datos_prueba(test_path, rgb=False, show=False):
-    # Definir el camino de las imágenes
-    path_imagenes_prueba = test_path
-    # Importar las imágenes en formato tif
-    X_test = io.imread(path_imagenes_prueba, plugin="tifffile")
+def datos_prueba(test_path, rgb=False, show=False, tiff=True):
+    """
+    Importa los datos de test_path y los normaliza.
+
+    Argumentos:
+        test_path: String que tiene el camino completo del directorio donde están las imágenes.
+        rgb: Booleano para reestructurar a forma de las imágenes si son de color o grises.
+        show: Booleano, si es verdadero muestra una imagen aleatoria de las que se están importando.
+        tiff: Booleando, si es verdadero es porque las imágenes están en formato .tif.
+
+    Regresa:
+        X_test: Arreglo de numpy que contiene las imágenes importadas.
+    """
+    if tiff:
+        # Importar las imágenes en formato tif
+        X_test = io.imread(test_path, plugin="tifffile")
+    else:
+        X_test = io.imread(test_path)
+
     # Reacomodar el arreglo para que tenga canales de color
     if rgb:
         # Se agregan los tres canales
@@ -69,27 +101,19 @@ def datos_prueba(test_path, rgb=False, show=False):
 
     return X_test
 
-
-def imagen_en_partes(x, y, size=(128, 128), num_partes=4):
-    """
-    Asumiendo que la imagen es (ancho, alto, canales)
-    """
-    x_patches = image.extract_patches_2d(
-        x[:, :, 0], patch_size=size, max_patches=num_partes, random_state=0
-    )
-    y_patches = image.extract_patches_2d(
-        y[:, :, 0], patch_size=size, max_patches=num_partes, random_state=0
-    )
-    # Reajustar tamaño
-    nuevo_tam = list(x_patches.shape) + [1]
-    x_patches = x_patches.reshape(tuple(nuevo_tam))
-    y_patches = y_patches.reshape(tuple(nuevo_tam))
-
-    return x_patches, y_patches
-
 def muchas_imagenes_en_partes(x, y=None, size=(128, 128), num_partes=4):
     """
-    Asumiendo que las imágenes tienen formato (ancho, alto, canales)
+    Toma dos arreglos de imágenes, x,y, y las separa en num_partes número de imágenes.
+
+    Argumentos:
+        x: Arreglo de numpy que pertenece a un conjunto de imágenes.
+        y: Arreglo de numpy que pertenece a un conjunto de imágenes.
+            (normalmente los mapas de segmentación de x); es opcional.
+        size: Tupla de dos elementos que contiene el tamaño de las imágenes resultantes.
+        num_partes: Entero que determina en cuántas partes se van a separar las imágenes.
+
+    Regresa:
+        x_imgs, y_imgs: Arreglos de numpy del conjunto de imágenes separadas en partes.
     """
     x_patches = image.PatchExtractor(patch_size=size, max_patches=num_partes, random_state=0)
     x_imgs = x_patches.transform(x)
@@ -108,6 +132,19 @@ def muchas_imagenes_en_partes(x, y=None, size=(128, 128), num_partes=4):
     return x_imgs
 
 def aumentar_imagenes_mascaras(x, y, batch_size=4, transformaciones=None, seed=6):
+    """
+    Toma dos conjuntos de imágenes y las transforma utilizando ImageDataGenerator de Keras.
+
+    Argumentos:
+        x: Arreglo de numpy que pertenece a un conjunto de imágenes.
+        y: Arreglo de numpy que pertenece a un conjunto de imágenes.
+        batch_size: Entero que determina el número de imágenes a transformar del total.
+        transformaciones: Diccionario con las transformaciones a realizar sobre las imágenes.
+        seed: Entero para fijar el generador de números aleatorios y hacerlo reproducible.
+
+    Regresa:
+        generador: Un generador de Python con los conjuntos de imágenes transformados.
+    """
     if transformaciones is None:
         transformaciones = dict(
         rotation_range=10.0,
@@ -126,4 +163,6 @@ def aumentar_imagenes_mascaras(x, y, batch_size=4, transformaciones=None, seed=6
     x_aumentado = datagen_x.flow(x, batch_size=batch_size, seed=seed)
     y_aumentado = datagen_y.flow(y, batch_size=batch_size, seed=seed)
 
-    return zip(x_aumentado, y_aumentado)
+    generador = zip(x_aumentado, y_aumentado)
+
+    return generador
