2e21bdf6c54d134e5ef86040f6cac81663634164
Edwin <developeredwin@gmail.com>
2019-09-11 18:57:52 -0500

    Updated documentation for the UNet tests.

--- tests/test_unet_model.py
+++ tests/test_unet_model.py
@@ -6,6 +6,9 @@ import numpy as np
 
 
 def test_unet_is_model():
+    """Test that the model is an actual Keras model,
+    and that the layers are valid ones.
+    """
 
     model = unet()
 
@@ -13,7 +16,10 @@ def test_unet_is_model():
 
 
 def test_unet_segmentation():
-
+    """Test that the UNet model can train correctly, and that it
+    returns a valid result.
+    """
+    # Import some sample images from within the directory
     x_train = skio.ImageCollection(
         "example_dataset/images_prepped_train/*.png"
     ).concatenate()
@@ -26,29 +32,26 @@ def test_unet_segmentation():
     y_test = skio.ImageCollection(
         "example_dataset/annotations_prepped_test/*.png"
     ).concatenate()
-
+    # Crop the images to 256x256 and convert them to float32
     x_train = x_train[:, :256, :256, :].astype(np.float32)
     y_train = y_train[:, :256, :256, None].astype(np.float32)
     x_test = x_test[:, :256, :256, :].astype(np.float32)
     y_test = y_test[:, :256, :256, None].astype(np.float32)
-
+    # Normalize the images
     x_train /= 255.0
     y_train /= 255.0
     x_test /= 255.0
     y_test /= 255.0
-
+    # Create the model and train it, test the results
     model = unet(input_size=(256, 256, 3))
-
     model.compile(
         loss=tf.keras.losses.BinaryCrossentropy(),
         optimizer=tf.keras.optimizers.Adam(),
         metrics=[tf.keras.metrics.Accuracy()],
     )
-
     model.fit(
         x=x_train, y=y_train, batch_size=1, epochs=2, validation_data=(x_test, y_test)
     )
-
     result = model.predict(x_test)
 
     assert result[0].shape == y_test[0].shape
