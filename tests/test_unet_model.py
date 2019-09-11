d484fb2683d48cd08f9343f1f2d137bdf006ad38
Edwin <developeredwin@gmail.com>
2019-09-10 23:27:12 -0500

    Creating a model and training it is implemented.
    Testing needs documentation.

--- tests/test_unet_model.py
+++ tests/test_unet_model.py
@@ -1,6 +1,8 @@
 import pytest
 import tensorflow as tf
 from models import unet
+import skimage.io as skio
+import numpy as np
 
 
 def test_unet_is_model():
@@ -12,29 +14,42 @@ def test_unet_is_model():
 
 def test_unet_classification():
 
-    # TODO: Change the dataset, this one cannot be used for this particular task
+    x_train = skio.ImageCollection(
+        "example_dataset/images_prepped_train/*.png"
+    ).concatenate()
+    y_train = skio.ImageCollection(
+        "example_dataset/annotations_prepped_train/*.png"
+    ).concatenate()
+    x_test = skio.ImageCollection(
+        "example_dataset/images_prepped_test/*.png"
+    ).concatenate()
+    y_test = skio.ImageCollection(
+        "example_dataset/annotations_prepped_test/*.png"
+    ).concatenate()
 
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    x_train = x_train[:, :256, :256, :].astype(np.float32)
+    y_train = y_train[:, :256, :256, tf.newaxis].astype(np.float32)
+    x_test = x_test[:, :256, :256, :].astype(np.float32)
+    y_test = y_test[:, :256, :256, tf.newaxis].astype(np.float32)
 
-    x_train = x_train[:, :, tf.newaxis]
-    x_test = x_test[:, :, tf.newaxis]
+    x_train /= 255.0
+    y_train /= 255.0
+    x_test /= 255.0
+    y_test /= 255.0
 
-    y_train = tf.keras.utils.to_categorical(y_train)
-    y_test = tf.keras.utils.to_categorical(y_test)
-
-    model = unet(input_size=(28, 28, 1))
+    model = unet(input_size=(256, 256, 3))
 
     model.compile(
-        loss=tf.keras.losses.CategoricalCrossentropy(),
+        loss=tf.keras.losses.BinaryCrossentropy(),
         optimizer=tf.keras.optimizers.Adam(),
         metrics=[tf.keras.metrics.Accuracy()],
     )
 
     model.fit(
-        x=x_train, y=y_train, batch_size=32, epochs=1, validation_data=(x_test, y_test)
+        x=x_train, y=y_train, batch_size=1, epochs=2, validation_data=(x_test, y_test)
     )
 
-    result = model.predict(x_test[0])
+    result = model.predict(x_test)
 
-    assert result.shape == y_test[0].shape
+    assert result[0].shape == y_test[0].shape
 
