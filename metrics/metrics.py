c1ec519ce3ecdf288ffef9048ee4e5016c1bd2c5
Edwin <developeredwin@gmail.com>
2019-09-11 19:33:24 -0500

    Added documentation for the metrics.

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -3,40 +3,41 @@ import tensorflow as tf
 
 def jaccard_index(y_true, y_pred):
     """
-    Métrica de Jaccard (intersección sobre unión, IoU) para determinar
-    la eficacia de la segmentación. Es una métrica personalizada para Keras.
+    Jaccard index that evaluates segmentation maps and its effectiveness. If the value is one,
+    the segmentation map predicted is exact.
 
-    Argumentos:
-        y_true: Arreglo de numpy o tensor de TensorFlow que contiene el resultado real.
-        y_true: Arreglo de numpy o tensor de TensorFlow que contiene el resultado obtenido del modelo.
+    Arguments:
+        y_true: TensorFlow Tensor with the ground truth.
+        y_pred: TensorFlow Tensor with the predicted value.
 
-    Regresa:
-        resultado: Un valor flotante proveniente de hacer la medición.
+    Returns:
+        resultado: Scalar that determines the segmentation error.
     """
     y_true_f = tf.reshape(y_true, shape=[-1])
     y_pred_f = tf.reshape(y_pred, shape=[-1])
-    interseccion = tf.reduce_sum(y_true_f * y_pred_f)
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
     union = tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f)
-    resultado = (interseccion + 1.0) / (union - interseccion + 1.0)
+    result = (intersection + 1.0) / (union - intersection + 1.0)
 
-    resultado = tf.reduce_mean(resultado)
+    result = tf.reduce_mean(result)
 
-    return resultado
+    return result
 
 
 def ternaus_loss(y_true, y_pred):
     """
-    Pérdida inspirada por TernausNet
+    Loss inspired by TernausNet
     https://arxiv.org/abs/1801.05746
     
-    Es una combinación entre entropía cruzada binaria y el logaritmo del índice de Jaccard.
+    A (hopefully) smooth and differentiable combination between binary cross-entropy
+    and the Jaccard index for better segmentation training.
 
-    Argumentos:
-        y_true: Arreglo de numpy o tensor de TensorFlow que contiene el resultado real.
-        y_true: Arreglo de numpy o tensor de TensorFlow que contiene el resultado obtenido del modelo.
+    Arguments:
+        y_true: TensorFlow Tensor with the ground truth.
+        y_pred: TensorFlow Tensor with the predicted value.
 
-    Regresa:
-        loss: Un valor flotante proveniente de hacer la medición.
+    Returns:
+        resultado: Scalar that determines the segmentation error.
     """
     loss = tf.keras.losses.binary_crossentropy(y_true, y_pred) - tf.log(
         jaccard_index(y_true, y_pred)
@@ -46,6 +47,17 @@ def ternaus_loss(y_true, y_pred):
 
 
 def dice_coef(y_true, y_pred, smooth=1.0):
+    """
+    The Sorensen-Dice coefficient is a close relative of the Jaccard index, and it should
+    almost always be logged simultaneously.
+
+    Arguments:
+        y_true: TensorFlow Tensor with the ground truth.
+        y_pred: TensorFlow Tensor with the predicted value.
+
+    Returns:
+        resultado: Scalar that determines the segmentation error.
+    """
 
     y_true_f = tf.reshape(y_true, shape=[-1])
     y_pred_f = tf.reshape(y_pred, shape=[-1])
