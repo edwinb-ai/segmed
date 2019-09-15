49915416db561d8fc6f38efc56b9fadd1710b685
Edwin <developeredwin@gmail.com>
2019-09-15 15:16:15 -0500

    :package: Fixed the setup.py file and added a new requirements.txt

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name="segnet",
-    version="0.1",
+    version="0.2",
     author="Edwin Bedolla",
     author_email="developeredwin@gmail.com",
-    packages=["segnet"],
+    packages=find_packages(),
     license="LICENSE",
+    url="https://github.com/DCI-NET/segnet",
     description="Applying Deep Learning to medical image segmentation tasks.",
     long_description=open("README.md").read(),
 )
