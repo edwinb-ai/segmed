8cd17ab007b6f6b3e3001c7437fb5a5f0cb387d3
Edwin <developeredwin@gmail.com>
2019-10-29 13:07:54 -0600

    :bookmark:
    Updated version to 0.5.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except:
 
 setup(
     name="segnet",
-    version="0.5",
+    version="0.5.1",
     author="Edwin Bedolla",
     author_email="developeredwin@gmail.com",
     packages=find_packages(),
