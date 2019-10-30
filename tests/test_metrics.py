1a2add3ca7299b83f5284ee7e883b1c783a14d75
Edwin <developeredwin@gmail.com>
2019-10-30 17:23:15 -0600

    :pencil:
    Added docstrings to tests for metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -8,18 +8,23 @@ import numpy as np
 class CreateSegmentations:
     @staticmethod
     def _create_segmentation_map():
-        # Extract a segmentation map in grayscale and normalize
+        """Take a segmentation map, normalize it and cast it to float32.
+        Then turn it to a TensorFlow tensor.
+        """
         segmap = skio.imread("tests/example_dataset/1.png", as_gray=True)
         segmap = np.array(segmap / 255.0).astype(np.float32)
-        # Use eager execution for TensorFlow
         segmap = tf.convert_to_tensor(segmap, dtype=tf.float32)
 
         return segmap
 
     def _create_ground_predicted(self):
+        """Create a segmentation map as ground truth and modify it
+        minimally so that it is considered a predicted truth.
+        """
         ground_truth = self._create_segmentation_map()
         pred_truth = skio.imread("tests/example_dataset/1.png", as_gray=True)
         pred_truth = np.array(pred_truth / 255.0).astype(np.float32)
+        # Change only to pixels in the image
         pred_truth[30, 40] = 0.0
         pred_truth[50, 60] = 0.0
         pred_truth = tf.convert_to_tensor(pred_truth, dtype=tf.float32)
@@ -50,6 +55,10 @@ class TestMetrics(CreateSegmentations):
         assert pytest.approx(result == [1.0])
 
     def test_o_rate(self):
+        """Test the over segmentation rate. In this case it should
+        be close to one as the ground and predicted truths are almost
+        equal.
+        """
         ground_truth, pred_truth = self._create_ground_predicted()
 
         result = metrics.o_rate(ground_truth, pred_truth).numpy()
@@ -57,6 +66,10 @@ class TestMetrics(CreateSegmentations):
         assert pytest.approx(result == [0.0])
 
     def test_u_rate(self):
+        """Test the under segmentation rate. In this case it should
+        be close to one as the ground and predicted truths are almost
+        equal.
+        """
         ground_truth, pred_truth = self._create_ground_predicted()
 
         result = metrics.u_rate(ground_truth, pred_truth).numpy()
@@ -64,6 +77,10 @@ class TestMetrics(CreateSegmentations):
         assert pytest.approx(result == [0.0])
 
     def test_err_rate(self):
+        """Test the overall segmentation rate. In this case it should
+        be close to one as the ground and predicted truths are almost
+        equal.
+        """
         ground_truth, pred_truth = self._create_ground_predicted()
 
         result = metrics.err_rate(ground_truth, pred_truth).numpy()
@@ -76,7 +93,6 @@ class TestLosses(CreateSegmentations):
         """Test that the Ternaus Loss evaluates to zero. Keep in mind
         that the result is an array, so we need to evaluate in a closed form.
         """
-
         segmap = self._create_segmentation_map()
 
         result = losses.ternaus_loss(segmap, segmap).numpy()
