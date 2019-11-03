0490cbdc8c64264581ae92537d9a579172088c93
Edwin <developeredwin@gmail.com>
2019-11-02 21:46:31 -0600

    :label:
    All the necessary type annotations for the utilities

--- segmed/utils/utils.py
+++ segmed/utils/utils.py
@@ -2,9 +2,12 @@ from skimage import io
 from sklearn.feature_extraction import image
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from typing import Optional, Tuple, Generator, Iterator, Union
 
 
-def extract_data(train_path, label_path=None, rgb=False):
+def extract_data(
+    train_path: str, label_path: Optional[str] = None, rgb: Optional[bool] = False
+) -> Tuple[np.ndarray, np.ndarray]:
     """Load images to memory from directories.
 
     Extracts data from train_path and label_path and returns a normalized
@@ -12,19 +15,19 @@ def extract_data(train_path, label_path=None, rgb=False):
     is just converting pixel values to a 0-1 range.
 
     Args:
-        train_path (str): String with the path for the full images.
-        label_path (str): String with the path for the segmentation maps.
-        rgb (bool): Boolean value to determine if the images are color or grayscale images.
+        train_path: String with the path for the full images.
+        label_path: String with the path for the segmentation maps.
+        rgb: Boolean value to determine if the images are color or grayscale images.
 
     Returns:
-        X_train, y_train (ndarray): Arrays of the images and their segmentation maps, normalized.
-        X_train (ndarray): Array of the images and their segmentation maps, normalized; when there
+        X_train, y_train: Arrays of the images and their segmentation maps, normalized.
+        X_train: Array of the images and their segmentation maps, normalized; when there
             is no `label_path`.
     """
 
     # Import images as a collection
     X_train = io.ImageCollection(train_path).concatenate()
-    
+
     # Reshape the array in case it is needed
     if rgb:
         # First check if for whatever reason the array is not RGB already
@@ -33,7 +36,7 @@ def extract_data(train_path, label_path=None, rgb=False):
     else:
         # If not RGB, just reshape to having just one channel, grayscale
         X_train = X_train[:, :, :, np.newaxis]
-    
+
     # Always convert to a valid type and normalize
     X_train = X_train.astype("float32")
     X_train /= 255.0
@@ -52,29 +55,41 @@ def extract_data(train_path, label_path=None, rgb=False):
     return X_train
 
 
-def split_images(x, y=None, size=(128, 128), num_part=4):
+def split_images(
+    x: np.ndarray,
+    y: Optional[np.ndarray] = None,
+    size: Tuple[int, int] = (128, 128),
+    num_part: Optional[int] = 4,
+) -> Tuple[np.ndarray, np.ndarray]:
     """Split images into smaller images.
 
     Takes two arrays of images, x,y, and splits them into num_part number
     of random patches.
 
     Args:
-        x: Numpy ndarray with images.
-        y: Numpy ndarray with images.
-        size: Tuple with two integer values, (height, width) of the resulting patches.
+        x: Array of images with shape
+            (number_of_images, height, width).
+        y: Array of images with shape
+            (number_of_images, height, width).
+        size: (height, width) of the resulting patches.
         num_part: Integer value; the number of resulting patches.
 
     Returns:
-        x_imgs, y_imgs: Numpy ndarrays with the patches of the original images.
+        x_imgs, y_imgs: The cropped patches from the original images.
+        x_imgs: The cropped patches from the original images, if only `x` is passed.
     """
-    x_patches = image.PatchExtractor(patch_size=size, max_patches=num_part, random_state=0)
+    x_patches = image.PatchExtractor(
+        patch_size=size, max_patches=num_part, random_state=0
+    )
     x_imgs = x_patches.transform(x)
     # Check if number of channels is the same for grayscale
     if x.shape[-1] != x_imgs.shape[-1]:
         x_imgs = x_imgs[:, :, :, np.newaxis]
 
     if not y is None:
-        y_patches = image.PatchExtractor(patch_size=size, max_patches=num_part, random_state=0)
+        y_patches = image.PatchExtractor(
+            patch_size=size, max_patches=num_part, random_state=0
+        )
         y_imgs = y_patches.transform(y)
 
         # Check if number of channels is the same for grayscale
@@ -85,7 +100,14 @@ def split_images(x, y=None, size=(128, 128), num_part=4):
 
     return x_imgs
 
-def image_mask_augmentation(x, y, batch_size=4, transformations=None, seed=6):
+
+def image_mask_augmentation(
+    x: np.ndarray,
+    y: np.ndarray,
+    batch_size: Optional[int] = 4,
+    transformations: Optional[dict] = None,
+    seed: Optional[int] = 6,
+) -> Iterator[Tuple[np.ndarray, np.ndarray]]:
     """Use Keras to transform images in several ways.
 
     Takes two sets of images and transforms it using ImageDataGenerator from Keras.
@@ -105,13 +127,13 @@ def image_mask_augmentation(x, y, batch_size=4, transformations=None, seed=6):
     # Always perform some basic transformations
     if transformations is None:
         transformations = dict(
-        rotation_range=10.0,
-        height_shift_range=0.02,
-        shear_range=5,
-        horizontal_flip=True,
-        vertical_flip=False,
-        fill_mode="constant"
-    )
+            rotation_range=10.0,
+            height_shift_range=0.02,
+            shear_range=5,
+            horizontal_flip=True,
+            vertical_flip=False,
+            fill_mode="constant",
+        )
 
     datagen_x = ImageDataGenerator(**transformations)
     datagen_x.fit(x, augment=True, seed=seed)
